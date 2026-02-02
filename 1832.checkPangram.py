99d13e811c1241c1f031a494affee4d8515122b5
shahalafathima213-alt <shahalafathima213@gmail.com>
2026-02-02 14:24:20 +0530

    1832-CheckPangram

---
+++ 1832.checkPangram.py
@@ -0,0 +1,24 @@
+# 1832. Check if the Sentence Is Pangram
+# Easy
+# Topics
+# premium lock icon
+# Companies
+# Hint
+# A pangram is a sentence where every letter of the English alphabet appears at least once.
+
+# Given a string sentence containing only lowercase English letters, return true if sentence is a pangram, or false otherwise.
+
+
+
+class Solution(object):
+    def checkIfPangram(self, sentence):
+        """
+        :type sentence: str
+        :rtype: bool
+        """
+        letters="abcdefghijklmnopqrstuvwxyz"
+        for n in letters:
+            if n not in sentence:
+                return False
+            else:
+                return True    
