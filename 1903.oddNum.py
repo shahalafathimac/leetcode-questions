d2d3883ee1ae32a699800179fcfd5d36fb6f4666
shahalafathima213-alt <shahalafathima213@gmail.com>
2026-02-23 10:39:53 +0530

    1903.oddNumber

---
+++ 1903.oddNum.py
@@ -0,0 +1,32 @@
+# 1903. Largest Odd Number in String
+# Solved
+# Easy
+# Topics
+# premium lock icon
+# Companies
+# Hint
+# You are given a string num, representing a large integer. Return the largest-valued odd integer (as a string) that is a non-empty substring of num, or an empty string "" if no odd integer exists.
+
+# A substring is a contiguous sequence of characters within a string.
+
+ 
+
+# Example 1:
+
+# Input: num = "52"
+# Output: "5"
+# Explanation: The only non-empty substrings are "5", "2", and "52". "5" is the only odd number
+
+
+class Solution(object):
+    def largestOddNumber(self, num):
+        """
+        :type num: str
+        :rtype: str
+        """
+        
+        for i in range(len(num) - 1, -1, -1):
+            if int(num[i]) % 2 != 0:
+                return num[:i+1]
+        return ""
+            
