f56316f3350ddfb3ed40f1a2472b6f31fd5f4634
Fathima Shahala C <shahalafathima213@gmail.com>
2026-05-09 09:41:27 +0530

    Time: 0 ms (100%), Space: 12.3 MB (55.56%) - LeetHub

---
+++ 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def createTargetArray(self, nums, index):
+        target=[]
+        for i in range(len(nums)):
+            target.insert(index[i],nums[i])
+        return target
+        
