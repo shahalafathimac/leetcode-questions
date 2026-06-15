01a4b6df95b9e9c266cf3889206c54740f88b9cf
Fathima Shahala C <shahalafathima213@gmail.com>
2026-06-15 19:46:21 +0530

    Time: 3 ms (55.14%), Space: 12.2 MB (89.61%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def mySqrt(self, x):
+        left, right = 1, x
+        ans = 0
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if mid * mid == x:
+                return mid
+
+            elif mid * mid < x:
+                ans = mid
+                left = mid + 1
+
+            else:
+                right = mid - 1
+
+        return ans
+        
